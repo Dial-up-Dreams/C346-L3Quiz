468f4419c72ef03d166a6bc37748e4b65140b22c
Shara <168702435+Dial-up-Dreams@users.noreply.github.com>
2024-10-29 00:12:10 +0800

    WHAT IS WRONG WITH YOU

--- App.js
+++ App.js
@@ -1,20 +1,90 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import RNPickerSelect from 'react-native-picker-select';
+import {Button, Text, ScrollView, Alert, Image} from "react-native";
+import {useState} from "react";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => {
+    const [ansO , SetAO] = useState("");
+    const [ansT , SetAT] = useState("");
+    const [ansTh , SetATH] = useState("");
+    return(
+        <ScrollView>
+            <Text>What number is this?</Text>
+            <Image
+                source={require('./img/74.png')}
+                style={{width:400, height:200}}
+                resizeMode="contain"
+            />
+            <RNPickerSelect
+                onValueChange={(value)=> SetAO(value)}
+                items={[
+                    {label:'15', value:'15'},
+                    {label:'74', value:'74'},
+                    {label:'42', value:'42'},
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+                ]}
+            />
+
+            <Text>What number is this?</Text>
+            <Image
+                source={require('./img/42.png')}
+                style={{width:400, height:200}}
+                resizeMode="contain"
+            />
+            <RNPickerSelect
+                onValueChange={(value)=> SetAT(value)}
+                items={[
+                    {label:'15', value:'15'},
+                    {label:'74', value:'74'},
+                    {label:'42', value:'42'},
+
+                ]}
+            />
+
+            <Text>What number is this?</Text>
+            <Image
+                source={require('./img/15.png')}
+                style={{width:400, height:200}}
+                resizeMode="contain"
+            />
+            <RNPickerSelect
+                onValueChange={(value)=> SetATH(value)}
+                items={[
+                    {label:'15', value:'15'},
+                    {label:'74', value:'74'},
+                    {label:'42', value:'42'},
+
+                ]}
+            />
+            <Button onPress={() => {
+                const SetAO ='74'
+                const SetAT ='42'
+                const SetATH ='15'
+                let message = 'no answer was entered.'
+                let score = 0
+                if (ansO === SetAO) {
+                    score+=1
+                }
+                if (ansT === SetAT) {
+                    score+=1
+                }
+                if (ansTh === SetATH) {
+                    score+=1
+                }
+                if (score === 1) {
+                    message = 'You have ' + score + '' + ' correct answer.'
+                    Alert.alert(message)
+                }
+                if (score > 1){
+                    message = 'You have ' + score + ' correct answers.'
+                     Alert.alert(message)
+                }
+                    }}
+                    title="Submit Answers"
+            />
+        </ScrollView>
+
+    );
+};
+
+export default App;
